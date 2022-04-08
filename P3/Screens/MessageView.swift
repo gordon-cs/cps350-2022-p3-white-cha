c562f1dfd4f62219c81ad6307cca6b5856cce05f
Silas White <silas.j.white@gmail.com>
2022-04-08 00:10:00 -0400

    Encrypt demo complete

--- P3/Screens/MessageView.swift
+++ P3/Screens/MessageView.swift
@@ -11,31 +11,22 @@ import SwiftUI
 struct MessageView: View {
     var body: some View {
         NavigationView{
-            
             VStack {
-                
                 //customizable nav bar
                 navBar()
-                    
                 ScrollView {
-                        
                         ForEach(0..<1, id: \.self) { num in
-                            Button {
-                                print("message cell")
+                            NavigationLink {
+                                ChatView(displayedText: "Example", showKey1: false, showKey2: false, encrypted: false)
                             } label: {
                                 messageCell()
                             }
-                            
                         }
-                        
                     }
                     .padding(.horizontal)
-                
             }
             .navigationBarHidden(true)
-            
         }
-        
     }
 }
 
